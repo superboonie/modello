eb6fdc0d71027faac51a6516c5b76f5df6f7976c
Boon Chew <boon@nanaimostudio.com>
2014-10-01 23:22:42 -0400

    changed to storyboard, designed UI

---
+++ Classes/ModelloAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  ELCImagePickerDemoAppDelegate.h
+//  ELCImagePickerDemo
+//
+//  Created by ELC on 9/9/10.
+//  Copyright 2010 ELC Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModelloAppDelegate : NSObject <UIApplicationDelegate> 
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ Classes/ModelloAppDelegate.m
@@ -0,0 +1,116 @@
+//
+//  ELCImagePickerDemoAppDelegate.m
+//  ELCImagePickerDemo
+//
+//  Created by ELC on 9/9/10.
+//  Copyright 2010 ELC Technologies. All rights reserved.
+//
+
+#import "ModelloAppDelegate.h"
+#import "ModelloMainViewController.h"
+#import <Parse/Parse.h>
+
+@implementation ModelloAppDelegate
+
+//Using generated synthesizers
+
+#pragma mark -
+#pragma mark Application lifecycle
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{    
+    // Override point for customization after application launch.
+    [Parse setApplicationId:@"0NxMtzbYZRIgRmVzNxu4cAdaL5TbvKWHqs46tLAv"
+                  clientKey:@"X4m6mQNA8nWZNZAZm6feTW9NVRbH7BT0kBUHeuEA"];
+    
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+    
+    // Register for Push Notitications, if running iOS 8
+    if ([application respondsToSelector:@selector(registerUserNotificationSettings:)]) {
+        UIUserNotificationType userNotificationTypes = (UIUserNotificationTypeAlert |
+                                                        UIUserNotificationTypeBadge |
+                                                        UIUserNotificationTypeSound);
+        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:userNotificationTypes
+                                                                                 categories:nil];
+        [application registerUserNotificationSettings:settings];
+        [application registerForRemoteNotifications];
+    } else {
+        // Register for Push Notifications before iOS 8
+        [application registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge |
+                                                         UIRemoteNotificationTypeAlert |
+                                                         UIRemoteNotificationTypeSound)];
+    }
+    
+    //self.window.rootViewController = _viewController;
+    //[self.window makeKeyAndVisible];
+
+    return YES;
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    // Store the deviceToken in the current installation and save it to Parse.
+    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+    [currentInstallation setDeviceTokenFromData:deviceToken];
+    [currentInstallation addUniqueObject:@"Modello" forKey:@"channels"];
+    [currentInstallation saveInBackground];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    [PFPush handlePush:userInfo];
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    /*
+     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+     */
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    /*
+     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+     If your application supports background execution, called instead of applicationWillTerminate: when the user quits.
+     */
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    /*
+     Called as part of  transition from the background to the inactive state: here you can undo many of the changes made on entering the background.
+     */
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    /*
+     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+     */
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    /*
+     Called when the application is about to terminate.
+     See also applicationDidEnterBackground:.
+     */
+}
+
+
+#pragma mark -
+#pragma mark Memory management
+
+- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application
+{
+    /*
+     Free up as much memory as possible by purging cached data objects that can be recreated (or reloaded from disk) later.
+     */
+}
+
+
+@end
